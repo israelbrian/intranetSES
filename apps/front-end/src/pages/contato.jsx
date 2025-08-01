3a556d552673c6301ae8831568e7a6e7588391d7
John Victor ou JV <153013106+ParreirasJuniorWeb@users.noreply.github.com>
2025-08-01 19:49:37 -0300

    feat: Adiciona página de contato com informações da equipe e gerador de assinatura ATUALIZAÇÃO_01_08_2025
    Este commit implementa a página de contato, que serve como um ponto central para os usuários acessarem informações de suporte e ferramentas úteis.
    
    Detalhes da implementação:
    - **Componentes Importados:** A página agora importa e utiliza os componentes `Forms_email` e `Arcodeao_teams`.
    - **Informações da Equipe:** Uma nova seção foi criada para listar os contatos (nome, e-mail e ramal) da equipe técnica de TI. Isso oferece uma forma direta de os usuários entrarem em contato com membros específicos.
    - **Formulário de Contato:** O componente `Forms_email` foi integrado à página, permitindo que os usuários preencham e enviem e-mails diretamente do site.
    - **Link para Assinatura:** Uma seção adicional foi incluída com um link externo para um gerador de assinatura de e-mail corporativa padrão da SES-MG. Isso ajuda os usuários que precisam criar ou atualizar sua assinatura digital.
    
    Essas adições melhoram a experiência do usuário ao consolidar os recursos de contato e suporte em uma única página, tornando-a mais completa e funcional.

--- apps/front-end/src/pages/contato.jsx
+++ apps/front-end/src/pages/contato.jsx
@@ -27,65 +27,72 @@ const Contato = () => {
           </div>
           {/* <!-- CO WORKING  --> */}
           <div className="col-md-6">
-            <Link to="/fhemig" className="text-decoration-none">
-              <div className="link-content">
-                {/* <!-- <h3 className="p-2 mt-2 display-6">E-mail equipe TI</h3>
+            <div className="link-content">
+              {/* <!-- <h3 className="p-2 mt-2 display-6">E-mail equipe TI</h3>
                         <p className="lead mt-4 text-primary ms-3">suporteti.ca@saude.mg.gov.br</p> --> */}
-                <h3 className="p-2 mt-2 display-6">
-                  Equipe Técnica / E-mail / Ramal{" "}
-                </h3>
-                <ul className="list-group mt-4">
-                  <li className="list-group-item">
-                    Alexander Spelta /
-                    <a href="mailto:alexander.spelta@saude.mg.gov.br">
-                      alexander.spelta@saude.mg.gov.br
-                    </a>
-                    / 31 3916-0031
-                  </li>
-                  <li className="list-group-item">
-                    Edinei Goncalves Araujo /
-                    <a href="mailto:edinei.araujo@saude.mg.gov.br">
-                      edinei.araujo@saude.mg.gov.br
-                    </a>
-                    / 31 3916-0031
-                  </li>
-                  <li className="list-group-item">
-                    Wesley Francisco Soares Fernandes /
-                    <a href="mailto:wesley.fernandes@saude.mg.gov">
-                      wesley.fernandes@saude.mg.gov
-                    </a>
-                    / 31 3916-0817
-                  </li>
-                  <li className="list-group-item">
-                    Pedro Nascimento Rodrigues /
-                    <a href="mailto:pedro.nascimento@saude.mg.gov">
-                      pedro.nascimento@saude.mg.gov
-                    </a>
-                    / 31 3916-0817
-                  </li>
-                  <li className="list-group-item">
-                    Israel Brian Pimenta Gonçalves Araújo /
-                    <a href="mailto:israel.goncalves@saude.mg.gov">
-                      israel.goncalves@saude.mg.gov
-                    </a>
-                    / 31 3916-0031
-                  </li>
-                  <li className="list-group-item">
-                    João Victor Parreiras Soares /
-                    <a href="mailto:joao.soares@saude.mg.gov.br">
-                      joao.soares@saude.mg.gov.br
-                    </a>
-                    / 31 3916-0031
-                  </li>
-                  <li className="list-group-item">
-                    E-mail equipe TI:{" "}
-                    <a href="mailto:suporteti.ca@saude.mg.gov.br">
-                      suporteti.ca@saude.mg.gov.br
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </Link>
+              <h3 className="p-2 mt-2 display-6">
+                Equipe Técnica / E-mail / Ramal{" "}
+              </h3>
+              <small>
+                ChatBot_ATI_SES:{" "}
+                <a
+                  href="https://app.chatvolt.ai/@chat_bot_ti_ses"
+                  target="_blank"
+                >
+                  Clique Aqui!
+                </a>
+              </small>
+              <ul className="list-group mt-4">
+                <li className="list-group-item">
+                  Alexander Spelta /
+                  <a href="mailto:alexander.spelta@saude.mg.gov.br">
+                    alexander.spelta@saude.mg.gov.br
+                  </a>
+                  / 31 3916-0031
+                </li>
+                <li className="list-group-item">
+                  Edinei Goncalves Araujo /
+                  <a href="mailto:edinei.araujo@saude.mg.gov.br">
+                    edinei.araujo@saude.mg.gov.br
+                  </a>
+                  / 31 3916-0031
+                </li>
+                <li className="list-group-item">
+                  Wesley Francisco Soares Fernandes /
+                  <a href="mailto:wesley.fernandes@saude.mg.gov">
+                    wesley.fernandes@saude.mg.gov
+                  </a>
+                  / 31 3916-0817
+                </li>
+                <li className="list-group-item">
+                  Pedro Nascimento Rodrigues /
+                  <a href="mailto:pedro.nascimento@saude.mg.gov">
+                    pedro.nascimento@saude.mg.gov
+                  </a>
+                  / 31 3916-0817
+                </li>
+                <li className="list-group-item">
+                  Israel Brian Pimenta Gonçalves Araújo /
+                  <a href="mailto:israel.goncalves@saude.mg.gov">
+                    israel.goncalves@saude.mg.gov
+                  </a>
+                  / 31 3916-0031
+                </li>
+                <li className="list-group-item">
+                  João Victor Parreiras Soares /
+                  <a href="mailto:joao.soares@saude.mg.gov.br">
+                    joao.soares@saude.mg.gov.br
+                  </a>
+                  / 31 3916-0031
+                </li>
+                <li className="list-group-item">
+                  E-mail equipe TI:{" "}
+                  <a href="mailto:suporteti.ca@saude.mg.gov.br">
+                    suporteti.ca@saude.mg.gov.br
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
           <br />
           <div className="col-md-6">
@@ -101,6 +108,31 @@ const Contato = () => {
       </section>
       <br />
       <Forms_email />
+      <br />
+      <section className="container p-5 mt-5 bg-white section-duvidas rounded-3">
+        <h1 className="display-6 text-center p-3 text-uppercase">
+          ASSINATURA DE E-MAIL CORPORATIVO PADRÃO SES - MG
+        </h1>
+        <div className="row">
+          <div className="col-md-6">
+            <div className="link-content">
+              <h4 className="p-2 mt-2 display-8 flex justify-content-center align-items-center">
+                Caso não possua uma assinatura de e-mail corporativa da SES -
+                MG. Clique aqui para gerar uma!
+              </h4>
+              <a
+                href="http://assinaturaemail.saude.mg.gov.br/"
+                target="_blank"
+                className="btn btn-primary p-2 m-3"
+              >
+                Criar Assinatura Digital
+              </a>
+              <br />
+            </div>
+          </div>
+        </div>
+      </section>
+      <br />
       <Footer />
     </>
   );
